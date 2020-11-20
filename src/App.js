bd337343fd2c877bcbdeecac113be64582603875
github username <jenniferagrenier@gmail.com>
2020-11-20 15:40:40 -0700

    card container component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import CardContainer from './components/CardContainer';
 
 function App() {
   return (
     <div className="App">
     <h1>Put your Ideas in here!</h1>
+      <CardContainer />
     </div>
   );
 }
---
+++ src/components/CardContainer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function cardContainer() {
+    return (
+        <div>
+            <h1>container for ideas
+            </h1>
+        </div>
+    )
+}
