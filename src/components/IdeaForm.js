28dd005a2a7dd96f3e47de8a957d618b5bf02326
AJ <alexanderjessediaz@gmail.com>
2020-12-16 13:52:44 -0700

    create idea form and style it

--- src/components/IdeaForm.js
+++ src/components/IdeaForm.js
@@ -2,10 +2,21 @@ import React from 'react'
 
 export default function IdeaForm() {
     return (
-        <div>
+        <section >
             <h2>Have a new idea? Add it here!</h2>
-            <input></input>
-            <button>Submit</button>
-        </div>
+            <form className="new-idea-form">
+                <label htmlFor="title">Title:</label>
+                <input name="title" id="title" type="text"></input>
+                <label htmlFor="category">Category:</label>
+                <input name="category" id="category"></input>
+                {/* needs to be a select */}
+                <label htmlFor="content">Content:</label>
+                <input name="content" id="content" type="text"></input>
+                <label htmlFor="date">Date:</label>
+                <input name="date" id="date" type="date"></input>
+                <input type="submit"></input>
+            </form>
+            
+        </section>
     )
 }
