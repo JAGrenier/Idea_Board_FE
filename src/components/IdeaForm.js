e08c0a89869eb91c9570fbfc5b0debf01076ed9d
AJ <alexanderjessediaz@gmail.com>
2020-12-16 14:14:21 -0700

    form onChange, newIdea object in form state, add updateIdea and addNewIdea

--- src/components/IdeaForm.js
+++ src/components/IdeaForm.js
@@ -1,22 +1,49 @@
-import React from 'react'
+import { Component } from 'react'
 
-export default function IdeaForm() {
-    return (
-        <section >
-            <h2>Have a new idea? Add it here!</h2>
-            <form className="new-idea-form">
-                <label htmlFor="title">Title:</label>
-                <input name="title" id="title" type="text"></input>
-                <label htmlFor="category">Category:</label>
-                <input name="category" id="category"></input>
-                {/* needs to be a select */}
-                <label htmlFor="content">Content:</label>
-                <input name="content" id="content" type="text"></input>
-                <label htmlFor="date">Date:</label>
-                <input name="date" id="date" type="date"></input>
-                <input type="submit"></input>
-            </form>
-            
-        </section>
-    )
+export default class IdeaForm extends Component {
+    
+    state = {
+        newIdea: {
+            title: "",
+            category: "",
+            content: "",
+            date: ""
+        }
+    }
+
+    addNewIdea = (event) => {
+        event.preventDefault()
+        
+    }
+
+    updateIdea = (event) => {
+        const key = event.target.name
+        const value = event.target.value
+
+        this.setState(state => {
+            state.newIdea[key] = value
+            return state
+        })
+    }
+    
+    render(){
+        return (
+            <section >
+                <h2>Have a new idea? Add it here!</h2>
+                <form className="new-idea-form" onSubmit={this.addNewIdea}>
+                    <label htmlFor="title">Title:</label>
+                    <input value={this.state.title} name="title" id="title" type="text" onChange={this.updateIdea}></input>
+                    <label htmlFor="category">Category:</label>
+                    <input name="category" id="category" onChange={this.updateIdea}></input>
+                    {/* needs to be a select */}
+                    <label htmlFor="content">Content:</label>
+                    <input name="content" id="content" type="text" onChange={this.updateIdea}></input>
+                    <label htmlFor="date">Date:</label>
+                    <input name="date" id="date" type="date" onChange={this.updateIdea}></input>
+                    <input type="submit"></input>
+                </form>
+                
+            </section>
+        )
+    }
 }
