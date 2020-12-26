68dc1393addcfa4a9b979ef7a8ea72dba8191b60
AJ <alexanderjessediaz@gmail.com>
2020-12-26 15:10:14 -0700

    Fixed form category

--- src/components/IdeaCard.js
+++ src/components/IdeaCard.js
@@ -3,41 +3,48 @@ import Delete from '../images/delete.svg'
 
 export default function IdeaCard({idea, deleteIdea}) {
     
-    
+
+    console.log(idea)
     const handleDelete = () =>{
         deleteIdea(idea.id)
     }
-
     
+    const categoryColors = () =>{
+     
+        if (idea.category === "Home"){
 
-    // switch (category) {
-    //     case home:
-            
-    //         break;
-
-    //     case app:
-
-    //         break;
-
-    //     case travel:
-
-    //         break;
-
-    //     case food:
-
-    //         break;
-
-    //     case gettingOutside:
-    //         break;
-
-    //     case other:
-    //         break;
+        }
+    }
+        // var className;
+        // switch (idea.category) {
+        //     case "Home":
+        //        return className="idea-card-home";
     
-    //     default:
-    //         break;
-    // }
+        //     case "app":
+        //         return  "idea-card-app"
+                
+    
+        //     case "travel":
+        //         return  "idea-card-travel"
+                
+    
+        //     case "food":
+        //         return  "idea-card-food"
+                
+    
+        //     case "gettingOutside":
+        //         return  "idea-card-getting-outside"
+                
+    
+        //     default:
+        //         return className="idea-card-other"
+        //         break;
+        // }
+
+
+//idea.category="home"? "idea-card-home": "idea-card-other"
     return (
-        <div className="idea-card">
+        <div className="idea-card-other">
             <h1>{idea.title}</h1>
             <h2>category: {idea.category}</h2>
             <p>{idea.content}</p>
